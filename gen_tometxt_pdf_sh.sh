4577bd24f92ca75b2bac9abbc3186636c786f1e8
nPeeech <33167904+nPeeech@users.noreply.github.com>
2022-12-06 15:34:47 +0900

    [update] 画像をpngからjpgに変更し、dpiを指定できるようにした

--- gen_tometxt_pdf_sh.sh
+++ gen_tometxt_pdf_sh.sh
@@ -2,14 +2,14 @@
 # ex. bash ./gen_tometxt_pdf_sh.sh hoge.pdf jpn 3840
 pdf="$1"
 lang="$2"
-scale="$3"
+dpi="$3"
 pdfwithoutext="${1%.pdf}"
 pdfdir="${pdfwithoutext}.d"
 
 mkdir "$pdfdir"
-pdftoppm -png -scale-to "${scale}" "${pdf}" "${pdfdir}/img"
+pdftoppm -jpeg -jpegopt quality=70 -r "${dpi}" "${pdf}" "${pdfdir}/img"
 
-find ./${pdfdir}/ -type f -name "*.png" | sed 's/\.png$//' | xargs -P4 -n1 -I{} tesseract {}.png {} -l "${lang}" pdf
+find ./${pdfdir}/ -type f -name "*.jpg" | sed 's/\.jpg$//' | xargs -P4 -n1 -I{} tesseract {}.jpg {} -l "${lang}" pdf
 
 pdftk "./${pdfdir}"/*.pdf cat output "${pdfwithoutext}_ocr.pdf"
 
